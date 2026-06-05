6b1be680df75f1d91a7bd365680def49e6b57d2a
pablokay860-sketch <pablokay860@gmail.com>
2026-06-05 18:01:04 +0300

    Move script.js to public folder for Railway deployment

---
+++ public/script.js
@@ -0,0 +1,23 @@
+async function loadBets() {
+  const container = document.getElementById('bets-container');
+  try {
+    const backendUrl = window.location.hostname === 'localhost' 
+      ? 'http://localhost:3000' 
+      : window.location.origin;
+    
+    const res = await fetch(`${backendUrl}/api/bets`);
+    const bets = await res.json();
+
+    container.innerHTML = bets.map(bet => `
+      <div class="bet">
+        <strong>${bet.event}</strong><br>
+        Home: ${bet.home} | Draw: ${bet.draw} | Away: ${bet.away}
+      </div>
+    `).join('');
+  } catch (err) {
+    container.innerHTML = 'Failed to load bets.';
+    console.error(err);
+  }
+}
+
+loadBets();
